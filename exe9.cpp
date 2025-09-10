5c6cd22d8431d6dccc3ac0831fd4fa34441b9368
ilsailxd <isag43480@gmail.com>
2025-09-10 10:27:31 -0300

    Update exe9.cpp

--- exe9.cpp
+++ exe9.cpp
@@ -1,36 +1,53 @@
-#include <stdio.h>
+#include<stdio.h>
+int primalidade (int numero);
 
-int primalidade(int numero) {
-    if (numero <= 1) {
+int main () {
+ int a, b;
+  printf("entre com o numero\n");
+  scanf("%i", &a);
+    b=primalidade(a);
+    if(b==0){
+        if(a%2==0){
+            printf("O numero eh primo\n");
+            printf("O numero %i eh PAR", a);
+            
+        } 
+        else{
+            printf("O numero eh primo\n");
+            printf("O numero %i eh IMPAR", a);
+        }
+    }
+    else{
+        if(a%2==0){
+            printf("O numero nao eh primo\n");
+            printf("O numero %i eh PAR", a);
+        }
+         else{
+            printf("O numero nao eh primo\n");
+            printf("O numero %i eh IMPAR", a);
+         }
+        }
+return 0;
+}
+int primalidade (int numero) {
+
+    if(numero<=1){
         return 1;
     }
-    else if (numero == 2) {
+    else if(numero==2){
         return 0;
     }
-    else if (numero % 2 == 0) {
+    else if(numero%2==0) {
         return 1;
     }
-    else {
-        int i;
-        for (i = 3; i < numero; ++i) {
-            if (numero % i == 0) {
-                return 1;
-            }
+    else{
+    int i;
+    for(i=3;i<numero;++i){
+        if(numero%i==0){
+            return 1; 
         }
     }
     return 0;
-}
-
-int main() {
-    int n;
-    printf("Digite um numero: ");
-    scanf("%d", &n);
-
-    if (primalidade(n) == 0) {
-        printf("%d eh primo\n", n);
-    } else {
-        printf("%d nao eh primo\n", n);
     }
 
-    return 0;
 }
