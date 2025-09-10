1c5a62be6f5451ade97ed6181d9e00c3d5752b10
ilsailxd <isag43480@gmail.com>
2025-09-10 10:23:24 -0300

    Update exe2.cpp

--- exe2.cpp
+++ exe2.cpp
@@ -1,31 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
+int conta (int numerador, int denominador);
 
-void conta(int numerador, int denominador);
-
-int main() {
+int main () {
     int a, b;
-
-    printf("Entre com o numerador:\n");
+    
+    printf("Entre com o numerador\n");
     scanf("%i", &a);
 
-    printf("Entre com o denominador:\n");
+    printf("Entre com o denominador\n");
     scanf("%i", &b);
-
     conta(a, b);
 
-    return 0;
+return 0;
 }
+int conta (int numerador, int denominador){
 
-void conta(int numerador, int denominador) {
-    int q = 0, cont;
-
-    while (numerador >= denominador) {
-        cont = numerador - denominador;
-        numerador = cont;
+    int q, cont;
+    while (numerador>0){
+        cont=numerador-denominador;
+        numerador=cont;
         q++;
+        if(numerador<=0){
+            printf("quociente = %i\nresto = %i", q, numerador*-1);
+        }
     }
-
-    if (numerador >= 0) {
-        printf("Quociente = %i\nResto = %i\n", q, numerador);
-    }
+return 0;
 }
